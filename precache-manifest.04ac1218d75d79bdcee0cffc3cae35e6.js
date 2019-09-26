e7df009995ffe4aa23292e6efd328dc890c9e6d3
cathe-zhang <zhangle_media@163.com>
2019-09-26 14:09:49 +0000

    deploy

---
+++ precache-manifest.04ac1218d75d79bdcee0cffc3cae35e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c8556c7435794f9a60dc2a8d09642d4",
+    "url": "/wx-cloud-demo/index.html"
+  },
+  {
+    "revision": "f4b8add4e19f148d8e3f",
+    "url": "/wx-cloud-demo/static/css/main.825511d3.chunk.css"
+  },
+  {
+    "revision": "8fd45af95d1cb680b9d2",
+    "url": "/wx-cloud-demo/static/js/2.b5b6fe38.chunk.js"
+  },
+  {
+    "revision": "f4b8add4e19f148d8e3f",
+    "url": "/wx-cloud-demo/static/js/main.17b0893e.chunk.js"
+  },
+  {
+    "revision": "6a28e3034cb3d4728798",
+    "url": "/wx-cloud-demo/static/js/runtime-main.2dcafb00.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/wx-cloud-demo/static/media/logo.25bf045c.svg"
+  }
+]);
